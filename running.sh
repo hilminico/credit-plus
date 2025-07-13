d7e48e3cb560a8eaddd2a540319afc25830ec1a3
hilmi <hilminico@gmail.com>
2025-07-13 14:38:11 +0700

    [NEW] make file sh for running main

---
+++ running.sh
@@ -0,0 +1,14 @@
+export DB_HOST=localhost
+export DB_USER=root
+export DB_PASSWORD=root
+export DB_NAME=user-service
+export DB_PORT=3306
+export SERVER_PORT=11000
+
+export APP_NAME=Krealogi Inovasi Digital
+export MAIL_FROM=noreply@krealogi-inovasi-digital.com
+export MAIL_PASSWORD=test
+export MAIL_HOST=smtp.example.com
+export MAIL_PORT=587
+
+go run ./cmd/main.go
