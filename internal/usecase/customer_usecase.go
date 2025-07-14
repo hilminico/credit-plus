a69d347971718a96b9be4bfd5226185400043a8d
hilmi <hilminico@gmail.com>
2025-07-15 01:02:43 +0700

    [NEW] register customer detail repo in customer_usecase.go and add function get customer, update customer detail

--- internal/usecase/customer_usecase.go
+++ internal/usecase/customer_usecase.go
@@ -2,23 +2,27 @@ package usecase
 
 import (
 	"context"
-	"creditPlus/helper/token"
 	"creditPlus/internal/domain"
 	"creditPlus/internal/repository"
 	"errors"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 	"os"
+	"time"
 )
 
 type CustomerService struct {
-	customerRepo *repository.CustomerRepository
+	customerRepo       *repository.CustomerRepository
+	customerDetailRepo *repository.CustomerDetailRepository
 }
 
 func NewCustomerService(
 	customerRepo *repository.CustomerRepository,
+	customerDetailRepo *repository.CustomerDetailRepository,
 ) *CustomerService {
 	return &CustomerService{
-		customerRepo: customerRepo,
+		customerRepo:       customerRepo,
+		customerDetailRepo: customerDetailRepo,
 	}
 }
 
@@ -34,14 +38,75 @@ func (s *CustomerService) Login(ctx context.Context, LoginRequest *domain.LoginR
 		return nil, errors.New("customer.password-miss-match")
 	}
 
-	// generate token access
-	jwtToken, err := token.CreateJWTToken(customer.UniqueIdentifier, customer.Email, os.Getenv("SECRET_KEY"))
+	// Create token
+	claims := &domain.CustomerClaims{
+		UniqueIdentifier: customer.UniqueIdentifier,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+		},
+	}
+
+	tokenJwt := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	token, err := tokenJwt.SignedString([]byte(os.Getenv("SECRET_KEY")))
+
+	return &domain.LoginResponse{
+		Email:    customer.Email,
+		JwtToken: token,
+	}, nil
+}
+
+func (s *CustomerService) GetCustomer(ctx context.Context, id string) (*domain.Customer, error) {
+	customer, err := s.customerRepo.FindByID(ctx, id)
 	if err != nil {
 		return nil, err
 	}
 
-	return &domain.LoginResponse{
-		Email:    customer.Email,
-		JwtToken: jwtToken,
-	}, nil
+	return customer, nil
+}
+
+func (s *CustomerService) UpdateCustomerDetail(ctx context.Context, id string, updateData *domain.CustomerDetailRequest) (*domain.CustomerDetail, error) {
+	customerDetail, err := s.customerDetailRepo.FindByCustomerID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	if customerDetail.ID == 0 {
+		err := s.customerDetailRepo.Create(ctx, &domain.CustomerDetail{
+			CustomerId:   id,
+			NIK:          updateData.NIK,
+			FullName:     updateData.FullName,
+			LegalName:    updateData.LegalName,
+			PlaceOfBirth: updateData.PlaceOfBirth,
+			DateBirth:    updateData.DateBirth,
+			Salary:       updateData.Salary,
+			ImageKtp:     updateData.ImageKtp,
+			ImageSelf:    updateData.ImageSelf,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+	} else {
+		customerDetail.NIK = updateData.NIK
+		customerDetail.FullName = updateData.FullName
+		customerDetail.LegalName = updateData.LegalName
+		customerDetail.PlaceOfBirth = updateData.PlaceOfBirth
+		customerDetail.DateBirth = updateData.DateBirth
+		customerDetail.Salary = updateData.Salary
+		customerDetail.ImageKtp = updateData.ImageKtp
+		customerDetail.ImageSelf = updateData.ImageSelf
+
+		err = s.customerDetailRepo.Update(ctx, customerDetail)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	customerDetail, err = s.customerDetailRepo.FindByCustomerID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return customerDetail, nil
 }
