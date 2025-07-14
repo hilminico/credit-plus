b475f2dbe8c62f681482f02f94bc10fdd6082f01
hilmi <hilminico@gmail.com>
2025-07-14 19:46:07 +0700

    [NEW] add running.sh for run without docker

--- running.sh
+++ running.sh
@@ -1,14 +1,8 @@
 export DB_HOST=localhost
 export DB_USER=root
 export DB_PASSWORD=root
-export DB_NAME=user-service
+export DB_NAME=credit-plus
 export DB_PORT=3306
-export SERVER_PORT=11000
-
-export APP_NAME=Krealogi Inovasi Digital
-export MAIL_FROM=noreply@krealogi-inovasi-digital.com
-export MAIL_PASSWORD=test
-export MAIL_HOST=smtp.example.com
-export MAIL_PORT=587
+export PORT=11000
 
 go run ./cmd/main.go
